352536fcb930958c4a9621ddc6617bc2e471ae7c
Lisa <misslisazhou@gmail.com>
2016-05-05 18:31:00 -1000

    complete multiples.js challenge

--- multiples.js
+++ multiples.js
@@ -6,7 +6,11 @@
 exports.sumOfAMultiple = function( n ) {
   var sum = 0;
 
-  // do your work here
+  for (var i = n - 1; i >= 0; i--) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum+=i;
+    }
+  }
 
   return sum;
 };
